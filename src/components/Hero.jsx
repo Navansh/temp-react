c8d7773698b7a1fe5345e217946d058f1db87560
Navansh <khandelwalnavansh1@gmail.com>
2023-12-10 07:55:58 +0530

    do
    Signed-off-by: Navansh <khandelwalnavansh1@gmail.com>

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import useModeStore from "../context/modeStore";
+import Send from "./hero/Send";
+import SendAction from "./hero/SendAction";
+import Verify from "./hero/Verify";
+import VerifyAction from "./hero/VerifyAction";
+
+const Hero = () => {
+  const { mode } = useModeStore();
+
+  console.log(mode);
+  return (
+    <div className="flex h-screen w-full justify-center items-center">
+      <div className="flex mf:flex-row flex-col items-start justify-between relative bottom-8 md:p-20 py-12 px-4">
+        {mode === "receive" && (
+          <>
+            <Send />
+            <SendAction />
+          </>
+        )}
+        {mode === "verify" && (
+          <>
+            <VerifyAction />
+            <Verify />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
